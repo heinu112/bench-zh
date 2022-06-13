18d67e9ab3f67b7d25fd9b5a8152f1a5e2e4a380
heinu112 <81513657+heinu112@users.noreply.github.com>
2022-06-13 18:30:44 +0800

    Update bench-zh.sh

--- bench-zh.sh
+++ bench-zh.sh
@@ -232,6 +232,7 @@ print_intro() {
     echo "-------------------- Bench.sh 脚本 作者: Teddysun 汉化:黑弩 -------------------"
     echo " 版本            : $(_green v2022-06-01)"
     echo " 使用方法              : $(_red "wget -qO- bench.sh | bash")"
+    echo ""
 }
 
 # Get System information
